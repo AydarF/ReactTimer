e68b34a4c9ca3bb2a475ac2cfb8421d11845aef5
Aydar Fayzullin <aydar_fayzullin@yahoo.com>
2017-03-07 12:04:59 -0500

    Add navigatin

---
+++ app/components/Timer.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render: function() {
+    return (
+      <div>This is Timer!</div>
+    	);
+  }	
+});
+
+module.exports = Timer;
