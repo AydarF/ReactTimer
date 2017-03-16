a31121c37294f87aefb8be6c47e7f4d61e235e45
Aydar Fayzullin <aydar_fayzullin@yahoo.com>
2017-03-16 13:34:41 -0400

    Finalize tests & add page title

--- app/tests/components/Controls.test.jsx
+++ app/tests/components/Controls.test.jsx
@@ -23,7 +23,7 @@ describe('render', () => {
    it('should render start when paused', () => {
 		var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused"/>);
 		var $el = $(ReactDOM.findDOMNode(controls));
-        var $pauseButton = $el.find('button:contains(Pause)');
+        var $pauseButton = $el.find('button:contains(Start)');
 
 		expect($pauseButton.length).toBe(1);
 	});
